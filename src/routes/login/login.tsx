34d849e248dcfda8787e9584840fdc2d990a370e
John <jmullany@live.com>
2023-03-17 13:05:45 -0500

    moved login side up a bit

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -71,136 +71,138 @@ const Login = (): JSX.Element => {
         }
       >
         <div className="w-96 self-center justify-self-center">
-          <div>
-            <img className="mb-6" src={standard_logo} alt="Standard Logo" />
-            <h2 className="text-3xl font-extrabold tracking-light text-gray-900">
-              POS^ Admin Portal
-            </h2>
-            <h3 className="mt-3 text-sm font-light text-gray-600">
-              Sign in to your account
-            </h3>
-          </div>
+          <div className="mb-8">
+            <div>
+              <img className="mb-6" src={standard_logo} alt="Standard Logo" />
+              <h2 className="text-3xl font-extrabold tracking-light text-gray-900">
+                POS^ Admin Portal
+              </h2>
+              <h3 className="mt-3 text-sm font-light text-gray-600">
+                Sign in to your account
+              </h3>
+            </div>
 
-          <div className="mt-6">
-            <form className="space-y-6" onSubmit={doLogin}>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-600"
-                >
-                  Email address
-                </label>
-                <div className="relative mt-1 rounded-md shadow-sm">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    disabled={isLoading}
-                    className={classNames(
-                      emailIsInvalid
-                        ? "focus:ring-red-600"
-                        : "focus:ring-indigo-600",
-                      "block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-                    )}
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    aria-describedby="email-error"
-                  />
-                  {emailIsInvalid && (
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                      <ExclamationCircleIcon
-                        className="h-5 w-5 text-red-500"
-                        aria-hidden="true"
-                      />
-                    </div>
-                  )}
-                </div>
-                {emailIsInvalid && (
-                  <p className="mt-1 text-sm text-red-600" id="email-error">
-                    Not a valid email address.
-                  </p>
-                )}
-              </div>
-
-              <div className="space-y-1">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-600"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    disabled={isLoading}
-                    className="block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-600"
-                  />
+            <div className="mt-6">
+              <form className="space-y-6" onSubmit={doLogin}>
+                <div>
                   <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-gray-800"
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-600"
                   >
-                    Remember me
+                    Email address
                   </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-sky-500 hover:text-sky-600"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
-              <div className="flex-row items-center justify-center">
-                <button
-                  type="submit"
-                  disabled={isLoading || emailIsInvalid}
-                  className={classNames(
-                    isLoading
-                      ? "bg-sky-600 cursor-not-allowed"
-                      : "bg-sky-500 cursor-pointer hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
-                    "inline-flex items-center shadow w-full justify-center rounded-md py-2 px-3 text-sm font-semibold text-white shadow-sm transition ease-in-out duration-150 cursor-pointer"
-                  )}
-                >
-                  <div className="mr-2 flex items-center text-white font-light">
-                    Sign in
+                  <div className="relative mt-1 rounded-md shadow-sm">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      disabled={isLoading}
+                      className={classNames(
+                        emailIsInvalid
+                          ? "focus:ring-red-600"
+                          : "focus:ring-indigo-600",
+                        "block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                      )}
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      aria-describedby="email-error"
+                    />
+                    {emailIsInvalid && (
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                        <ExclamationCircleIcon
+                          className="h-5 w-5 text-red-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    )}
                   </div>
-                </button>
-                <div className="flex items-center justify-center mt-2">
-                  <Transition show={isLoading}>
-                    <Transition.Child
-                      enter="transition-opacity ease-linear duration-300"
-                      enterFrom="opacity-0"
-                      enterTo="opacity-100"
-                      leave="transition-opacity ease-linear duration-300"
-                      leaveFrom="opacity-100"
-                      leaveTo="opacity-0"
-                    >
-                      <ClipLoader size={25} color={"#36d7b7"} />
-                    </Transition.Child>
-                  </Transition>
+                  {emailIsInvalid && (
+                    <p className="mt-1 text-sm text-red-600" id="email-error">
+                      Not a valid email address.
+                    </p>
+                  )}
                 </div>
-              </div>
-            </form>
+
+                <div className="space-y-1">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-600"
+                  >
+                    Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      disabled={isLoading}
+                      className="block w-full rounded-md border-0 px-2 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <input
+                      id="remember-me"
+                      name="remember-me"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-600"
+                    />
+                    <label
+                      htmlFor="remember-me"
+                      className="ml-2 block text-sm text-gray-800"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+
+                  <div className="text-sm">
+                    <a
+                      href="#"
+                      className="font-medium text-sky-500 hover:text-sky-600"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                </div>
+
+                <div className="flex-row items-center justify-center">
+                  <button
+                    type="submit"
+                    disabled={isLoading || emailIsInvalid}
+                    className={classNames(
+                      isLoading
+                        ? "bg-sky-600 cursor-not-allowed"
+                        : "bg-sky-500 cursor-pointer hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+                      "inline-flex items-center shadow w-full justify-center rounded-md py-2 px-3 text-sm font-semibold text-white shadow-sm transition ease-in-out duration-150 cursor-pointer"
+                    )}
+                  >
+                    <div className="mr-2 flex items-center text-white font-light">
+                      Sign in
+                    </div>
+                  </button>
+                  <div className="flex items-center justify-center mt-2">
+                    <Transition show={isLoading}>
+                      <Transition.Child
+                        enter="transition-opacity ease-linear duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="transition-opacity ease-linear duration-300"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                      >
+                        <ClipLoader size={25} color={"#36d7b7"} />
+                      </Transition.Child>
+                    </Transition>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
         <div className="hidden lg:inline-block max-h-full my-3">
