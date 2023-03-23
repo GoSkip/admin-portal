33603bf57195eaa620dd9776dd3daf3bfbd0dd6c
Austin Benesh <austin@getskip.com>
2023-03-23 10:51:26 -0600

    Finished retailer selector for mobile mode

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -131,11 +131,92 @@ const Layout = (): JSX.Element => {
                   </div>
                 </Transition.Child>
                 <div className="flex flex-shrink-0 items-center px-4">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=300"
-                    alt="Skip Logo"
-                  />
+                  <Listbox
+                    value={session.active_retailer}
+                    onChange={setActiveRetailer}
+                  >
+                    {({ open }) => (
+                      <>
+                        <div className="relative mt-2 w-full">
+                          <Listbox.Button className="cursor-pointer relative w-full cursor-default bg-blue-600 py-2 px-2 text-left text-gray-200 text-lg sm:text-sm sm:leading-6 hover:bg-blue-700">
+                            <span className="flex items-center justify-between">
+                              <img
+                                src={session.active_retailer.image_url}
+                                alt=""
+                                className="h-10 w-10 rounded-full"
+                              />
+                              <span className="block truncate text-center">
+                                {session.active_retailer.name}
+                              </span>
+                              <ChevronUpDownIcon className="h-4 w-4" />
+                            </span>
+                          </Listbox.Button>
+                          <Transition
+                            show={open}
+                            as={Fragment}
+                            leave="transition ease-in duration-100"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Listbox.Options className="absolute z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                              {sortedRetailers.map((retailer: Retailer) => (
+                                <Listbox.Option
+                                  key={retailer.name}
+                                  className={({ active }) =>
+                                    classNames(
+                                      active
+                                        ? "bg-blue-600 text-white"
+                                        : "text-gray-900",
+                                      "relative cursor-default select-none py-2 pl-3 pr-9"
+                                    )
+                                  }
+                                  value={retailer}
+                                >
+                                  {({ selected, active }) => (
+                                    <>
+                                      <div className="flex items-center">
+                                        <img
+                                          src={retailer.image_url}
+                                          alt=""
+                                          className="h-5 w-5 flex-shrink-0 rounded-full"
+                                        />
+                                        <span
+                                          className={classNames(
+                                            selected
+                                              ? "font-semibold"
+                                              : "font-normal",
+                                            "ml-3 block truncate"
+                                          )}
+                                        >
+                                          {retailer.name}
+                                        </span>
+                                      </div>
+
+                                      {selected ? (
+                                        <span
+                                          className={classNames(
+                                            active
+                                              ? "text-white"
+                                              : "text-blue-600",
+                                            "absolute inset-y-0 right-0 flex items-center pr-4"
+                                          )}
+                                        >
+                                          <CheckIcon
+                                            className="h-5 w-5"
+                                            aria-hidden="true"
+                                          />
+                                        </span>
+                                      ) : null}
+                                    </>
+                                  )}
+                                </Listbox.Option>
+                              ))}
+                            </Listbox.Options>
+                          </Transition>
+                        </div>
+                      </>
+                    )}
+                  </Listbox>
                 </div>
                 <div className="mt-5 h-0 flex-1 overflow-y-auto">
                   <nav className="space-y-1 px-2">
@@ -388,11 +469,6 @@ const Layout = (): JSX.Element => {
 
         <main>
           <div className="py-6">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <h1 className="text-2xl font-semibold text-gray-900">
-                Dashboard
-              </h1>
-            </div>
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <LoadingProvider noBlur={false}>
                 <Outlet />
