63c2e14b63e8014d9f484527943a20a91403a145
Austin Benesh <austin@getskip.com>
2023-04-21 16:56:27 -0600

    got more work done

--- src/routes/kiosks/detail/index.tsx
+++ src/routes/kiosks/detail/index.tsx
@@ -25,8 +25,9 @@ import {
   LoadingContext,
   LoadingContextType,
 } from "../../../contexts/LoadingContext";
+import IpadCard from "./ipadCard";
 
-type KioskDetailsForm = {
+export type KioskDetailsForm = {
   kioskId: string;
   kioskDescription: string;
   terminalId: Option | null;
@@ -162,7 +163,7 @@ const KioskDetails = (): JSX.Element => {
       setIsLoading(false);
     }
   }, [kioskIsLoading, storeIsLoading]);
-  
+
   console.log(formState);
 
   return (
@@ -334,46 +335,12 @@ const KioskDetails = (): JSX.Element => {
         <StoreDetailsCard />
       </div>
       <div className="mt-8 grid grid-cols-4">
-        <div className="bg-white rounded-lg shadow-sm p-6 col-span-4 sm:col-span-3">
-          <h2 className="text-xl font-medium text-gray-900">iPad Details</h2>
-          <p className="mt-1 text-gray-500">
-            Hardware and software details last reported to the MDM service
-          </p>
-          <hr className="my-4 border-gray-200" />
-          <div className="flex flex-col justify-center items-center">
-            <ComputerDesktopIcon className="h-12 w-12 flex-shrink-0 text-blue-500 mt-8" />
-            <p className="text-sm font-semibold mt-2">
-              No iPad linked to this kiosk
-            </p>
-            <p className="text-sm text-gray-500 mt-2">
-              Get started by entering the iPad serial number
-            </p>
-            <div className="mt-4 w-1/2 grid grid-cols-4">
-              {enterSerialNoMode ? (
-                <>
-                  <div className="col-span-4 sm:col-span-3">
-                    <TextInput
-                      label="Serial number"
-                      value={formState.ipadSerial}
-                      htmlId={"ipadSerial"}
-                      onChange={handleInputChange}
-                    />
-                  </div>
-                  <div className="col-span-1 mt-1 ml-2">
-                    <PrimaryButton label="Submit" onClick={() => {}} />
-                  </div>
-                </>
-              ) : (
-                <div className="col-span-4 flex justify-center">
-                  <SecondaryButton
-                    label="Enter serial number"
-                    onClick={() => setEnterSerialNoMode(true)}
-                  />
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
+        <IpadCard
+          enterSerialNoMode={enterSerialNoMode}
+          setEnterSerialNoMode={setEnterSerialNoMode}
+          formState={formState}
+          handleInputChange={handleInputChange}
+        />
         <MetadataCard />
       </div>
     </div>
