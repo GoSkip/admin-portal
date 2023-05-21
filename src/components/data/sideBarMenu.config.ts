74d1726199a381ccec3d79031f956204b9df41b6
John Mullany <john.mullany@standard.ai>
2023-05-21 11:28:32 -0500

    fix submenu need css clean up

---
+++ src/components/data/sideBarMenu.config.ts
@@ -0,0 +1,52 @@
+import {
+    ChartBarIcon,
+    ComputerDesktopIcon,
+    CircleStackIcon,
+    TagIcon,
+    UserCircleIcon,
+    BuildingLibraryIcon,
+} from "@heroicons/react/24/outline";
+
+export interface MenuItem {
+    id: number;
+    label: string;
+    to?: string;
+    Icon?: any;
+    children?: MenuItem[];
+  };
+
+export const menuItems = [
+    { id: 1, label: "Reports", to: "/reports", Icon: ChartBarIcon },
+    { id: 2, label: "Kiosks", to: "/kiosks", Icon: ComputerDesktopIcon },
+    { id: 3, label: "Products", to: "/products", Icon: CircleStackIcon },
+    { id: 4, label: "Promotions", to: "/promotions", Icon: TagIcon },
+    {
+        id: 5,
+        label: "People",
+        Icon: UserCircleIcon,
+        children: [
+            { id: 51, label: "Clerks", to: "/people/clerks" },
+            { id:52, label: "Users", to: "/people/users" },
+        ],
+    },
+    {
+        id: 6,
+        label: "Administration",
+        to: "/admin",
+        Icon: BuildingLibraryIcon,
+        children: [
+            { id: 61, label: "Files", to: "/admin/files" },
+            { id: 62, label: "Taxes", to: "/admin/taxes" },
+            { id: 63, label: "Loyalty", to: "/admin/loyalty" },
+            { id: 64, label: "Payments", to: "/admin/payments" },
+            { id: 65, label: "Stores", to: "/admin/stores" },
+            { id: 66, label: "Retailer", to: "/admin/retailer" },
+        ],
+    },
+];
+
+export const userNavigation = [
+    { label: "Your Profile", to: "#" },
+    { label: "Settings", to: "#" },
+    { label: "Sign Out", to: "/logout" },
+];
--- src/components/sideBarItem.tsx
+++ src/components/sideBarItem.tsx
@@ -1,51 +1,62 @@
-import { useState } from "react";
-import { ChevronUpIcon } from "@heroicons/react/20/solid";
-import { useNavigate } from "react-router-dom";
 
-export default function SideBarItem({ item }: any) {
-    const [open, setOpen] = useState(false);
-    const navigate = useNavigate();
 
-    if (item.children) {
-        return (
-            <li>
-                <button
-                    className={
-                        "flex items-center w-full p-2 text-base font-normal text-[#4b5563] transition duration-200 border-transparent border-l-4 hover:border-l-4 hover:border-[#0284c7] hover:text-[#0284c7] hover:bg-[#f0f9ff]"
-                    }
-                    onClick={() => setOpen(!open)}
-                >
-                    <item.Icon className="flex-shrink-0 w-6 h-6 mr-5" />
-                    {item.label}
-                    <ChevronUpIcon
-                        className={open ? "w-6 h-6 ml-auto" : "w-6 h-6 rotate-180 ml-auto"}
-                    />
-                </button>
-                <ul className={open ? "py-2 space-y-2" : "hidden"}>
-                    {item.children.map((child: any, index: any) => (
-                        <li key={index}>
-                            <button
-                                className={`flex items-center w-full p-2  text-base font-normal text-[#4b5563] transition duration-200 border-transparent border-l-4 hover:border-l-4 hover:border-[#0284c7] hover:text-[#0284c7] hover:bg-[#f0f9ff]`}
-                                onClick={() => navigate(child.to)}
-                            >
-                                <span className={"ml-16"}>{child.label}</span>
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </li>
-        );
-    } else {
-        return (
-            <li>
-                <button
-                    className="flex items-center w-full p-2 text-base font-normal text-[#4b5563] transition duration-200 border-transparent border-l-4 hover:border-l-4 hover:border-[#0284c7] hover:text-[#0284c7] hover:bg-[#f0f9ff]"
-                    onClick={() => navigate(item.to)}
-                >
-                    <item.Icon className="flex-shrink-0 w-6 h-6 mr-4" />
-                    {item.label}
-                </button>
-            </li>
-        );
-    }
-}
+// import { useState } from "react";
+// import { ChevronUpIcon } from "@heroicons/react/20/solid";
+// import { useNavigate } from "react-router-dom";
+
+// export default function SideBarItem({ item }: any) {
+//     const [selectedItemId, setSelectedItemId] = useState(1);
+//     const isItemSelected = selectedItemId === item.id;
+//     const [open, setOpen] = useState(false);
+//     const navigate = useNavigate();
+
+//     const handleMenuItemClick = (itemId: number, to: string) => {
+//         setSelectedItemId(itemId);
+//         navigate(to);
+//     }
+
+//     if (item.children) {
+//         return (
+//             <li>
+//                 <button
+//                     className={
+//                         "flex items-center w-full p-2 text-base font-normal text-[#4b5563] transition duration-200 border-transparent border-l-4 hover:border-l-4 hover:border-[#0284c7] hover:text-[#0284c7] hover:bg-[#f0f9ff]"
+//                     }
+//                     onClick={() => setOpen(!open)}
+//                 >
+//                     <item.Icon className="flex-shrink-0 w-6 h-6 mr-5" />
+//                     <span key={item.children.id} className={`menu-item ${isItemSelected && selectedItemId === item.children.id ? "" : ""}`}>
+//                     {item.label}
+//                     </span>
+//                     <ChevronUpIcon
+//                         className={open ? "w-6 h-6 ml-auto" : "w-6 h-6 rotate-180 ml-auto"}
+//                     />
+//                 </button>
+//                 <ul className={open ? "py-2 space-y-2" : "hidden"}>
+//                     {item.children.map((child: any, index: any) => (
+//                         <li key={index}>
+//                             <button
+//                                 className={`flex items-center w-full p-2  text-base font-normal text-[#4b5563] transition duration-200 border-transparent border-l-4 hover:border-l-4 hover:border-[#0284c7] hover:text-[#0284c7] hover:bg-[#f0f9ff]`}
+//                                 onClick={() => handleMenuItemClick(child.id, child.to)}
+//                             >
+//                                 <span className={`menu-item ml-16 ${isItemSelected && selectedItemId === item.children.id ? "" : ""}`}>{child.label}</span>
+//                             </button>
+//                         </li>
+//                     ))}
+//                 </ul>
+//             </li>
+//         );
+//     } else {
+//         return (
+//             <li>
+//                 <button
+//                     className="flex items-center w-full p-2 text-base font-normal text-[#4b5563] transition duration-200 border-transparent border-l-4 hover:border-l-4 hover:border-[#0284c7] hover:text-[#0284c7] hover:bg-[#f0f9ff]"
+//                     onClick={() => navigate(item.to)}
+//                 >
+//                     <item.Icon className="flex-shrink-0 w-6 h-6 mr-4" />
+//                     {item.label}
+//                 </button>
+//             </li>
+//         );
+//     }
+// }
