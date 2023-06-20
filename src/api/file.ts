bc2e5e5665bd1c030fa75c6f03b1806339050020
Hamdi Mohamed <31364936+x0911@users.noreply.github.com>
2023-06-20 14:13:15 +0300

    wip: skip-table

--- src/api/file.ts
+++ src/api/file.ts
@@ -1,26 +1,57 @@
 import axios from "axios";
-export type GetFilesProps = {
+export type GetStoreFilesProps = {
+  jwt: string;
+  storeId: number;
+  limit: number;
+  page: number;
+};
+export type GetRetailerFilesProps = {
   jwt: string;
   retailerId: number;
   limit: number;
   page: number;
 };
 
-export const fetchFiles = async ({
+export const fetchStoreFiles = async ({
   jwt,
   limit,
   page,
-  retailerId,
-}: GetFilesProps) => {
+  storeId,
+}: GetStoreFilesProps) => {
   const domain = import.meta.env.VITE_ONBOARDING_DOMAIN;
   const query = {
-    store_id: retailerId,
+    store_id: storeId,
     product: "sco",
     begin_time: "2023-06-01T21:00:00.000Z",
     end_time: "2023-06-30T21:00:00.000Z",
   };
   const response = await axios.get(
     `${domain}/v1/file/store?query=${JSON.stringify(query)}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${jwt}`,
+      },
+    }
+  );
+  return response;
+};
+
+export const fetchRetailerFiles = async ({
+  jwt,
+  limit,
+  page,
+  retailerId,
+}: GetRetailerFilesProps) => {
+  const domain = import.meta.env.VITE_ONBOARDING_DOMAIN;
+  const query = {
+    retailer_id: retailerId,
+    product: "sco",
+    begin_time: "2023-06-01T21:00:00.000Z",
+    end_time: "2023-06-30T21:00:00.000Z",
+  };
+  const response = await axios.get(
+    `${domain}/v1/file/retailer?query=${JSON.stringify(query)}`,
     {
       headers: {
         "Content-Type": "application/json",
